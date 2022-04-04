6bbc6e85f3f060bb57d1d0d6b9bbff6caa696b13
Zoltan Arvai <zoltan.arvai@live.com>
2022-04-04 18:56:29 +0200

    refactor: move responsibilities of bluetooth connect and scan into the scan component

--- app/components/Views/LedgerConnect/Scan.tsx
+++ app/components/Views/LedgerConnect/Scan.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Alert, View, StyleSheet, ActivityIndicator, Linking, Platform } from 'react-native';
+import { Observable, Subscription } from 'rxjs';
 import TransportBLE from '@ledgerhq/react-native-hw-transport-ble';
-import { Observable } from 'rxjs';
 import { Device } from '@ledgerhq/react-native-hw-transport-ble/lib/types';
-import Text from '../../../components/Base/Text';
-import { deviceHeight, deviceWidth } from '../../../util/scaling';
+import { check, checkMultiple, PERMISSIONS, openSettings } from 'react-native-permissions';
+import { BleManager, State } from 'react-native-ble-plx';
+
+import SelectComponent from '../../UI/SelectComponent';
+import { deviceWidth } from '../../../util/scaling';
 import { mockTheme, useAppThemeFromContext } from '../../../util/theme';
 import { Colors } from '../../../util/theme/models';
-import SelectComponent from '../../UI/SelectComponent';
-import StyledButton from '../../../components/UI/StyledButton';
+import { handleAndroidBluetoothPermissions, handleIOSBluetoothPermission } from './ledgerUtils';
 
 const createStyles = (colors: Colors) =>
 	StyleSheet.create({
@@ -27,36 +29,13 @@ const createStyles = (colors: Colors) =>
 			height: 45,
 			width: deviceWidth * 0.85,
 		},
-		buttonContainer: {
-			position: 'absolute',
-			display: 'flex',
-			bottom: deviceHeight * 0.025,
-			left: 0,
-			width: '100%',
-		},
 	});
 
 const Scan = ({ onDeviceSelected }: { onDeviceSelected: (device: Device) => void }) => {
-	const [devices, setDevices] = useState<Device[]>([]);
-	const [currentDevice, setCurrentDevice] = useState<Device>(null);
-	const [error, setError] = useState<unknown | null>(null);
 	const { colors } = useAppThemeFromContext() || mockTheme;
-
 	const styles = useMemo(() => createStyles(colors), [colors]);
-
-	const startScan = useCallback(() => {
-		const subscription = new Observable(TransportBLE.listen).subscribe({
-			next: (e) => {
-				const deviceFound = devices.some((i) => i.id === e.descriptor.id);
-				e.type === 'add' && !deviceFound && setDevices([...devices, e?.descriptor]);
-			},
-			error: (_error) => {
-				setError(_error);
-			},
-		});
-
-		return subscription;
-	}, [devices]);
+	const [devices, setDevices] = useState<Device[]>([]);
+	const [canScan, setCanScan] = useState<boolean>(false);
 
 	const options = devices?.map(({ id, name, ...rest }: Partial<Device>) => ({
 		key: id,
@@ -66,30 +45,107 @@ const Scan = ({ onDeviceSelected }: { onDeviceSelected: (device: Device) => void
 	}));
 
 	useEffect(() => {
-		if (devices.length > 0 && !currentDevice) {
-			setCurrentDevice(devices[0]);
-		}
-	}, [devices, currentDevice]);
+		// Monitoring for the BLE adapter to be turned on
+		const manager = new BleManager();
+		const subscription = manager.onStateChange((state) => {
+			console.log('>>>> WE ARE HERE', state);
+			if (state === State.PoweredOff) {
+				Alert.alert('Bluetooth is off', 'Please turn on bluetooth for your device', [
+					{
+						text: 'Open Settings',
+						onPress: async () => {
+							Platform.OS === 'ios'
+								? Linking.openURL('App-Prefs:Bluetooth')
+								: Linking.sendIntent('android.settings.BLUETOOTH_SETTINGS');
+						},
+					},
+				]);
+			}
+		}, true);
 
-	const onSelect = async () => {
-		try {
-			await onDeviceSelected(currentDevice);
-		} catch (_error) {
-			setError(_error);
-		}
-	};
-
-	const onChange = async (_device: Device) => {
-		setCurrentDevice(_device);
-	};
+		return () => subscription.remove();
+	}, []);
 
 	useEffect(() => {
-		const subscription = startScan();
+		// Checking if app has required permissions
+		const run = async () => {
+			if (Platform.OS === 'ios') {
+				const bluetoothPermissionStatus = await check(PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL);
+				const bluetoothAllowed = handleIOSBluetoothPermission(bluetoothPermissionStatus);
+
+				if (bluetoothAllowed) {
+					setCanScan(true);
+				} else {
+					Alert.alert('Access blocked', 'Please enable bluetooth for you app', [
+						{
+							text: 'Open Settings',
+							onPress: async () => {
+								await openSettings();
+							},
+						},
+					]);
+				}
+			}
+
+			if (Platform.OS === 'android') {
+				const requiredPermissions = await checkMultiple([
+					PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+					PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+					PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+				]);
+
+				const bluetoothAllowed = handleAndroidBluetoothPermissions(requiredPermissions);
+
+				if (bluetoothAllowed) {
+					setCanScan(true);
+				} else {
+					Alert.alert(
+						'Missing permissions',
+						'Make sure you enable Bluetooth and Location access in your device settings',
+						[
+							{
+								text: 'Open Settings',
+								onPress: async () => {
+									await openSettings();
+								},
+							},
+						]
+					);
+				}
+			}
+		};
+
+		run();
+	}, []);
+
+	useEffect(() => {
+		// Initiate scanning and pairing if bluetooth is enabled
+		let subscription: Subscription;
+
+		if (canScan) {
+			subscription = new Observable(TransportBLE.listen).subscribe({
+				next: (e: any) => {
+					const deviceFound = devices.some((d) => d.id === e.descriptor.id);
+
+					if (e.type === 'add' && !deviceFound) {
+						setDevices([...devices, e?.descriptor]);
+						onDeviceSelected(e.descriptor);
+					}
+				},
+				error: (_error) => {
+					Alert.alert(
+						'Error while scanning the device',
+						'Please make sure your device is unlocked and the Ethereum app is running'
+					);
+				},
+			});
+		}
 
 		return () => {
 			subscription?.unsubscribe();
 		};
-	}, [startScan]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [canScan]);
 
 	return (
 		<View style={styles.container}>
@@ -99,21 +155,13 @@ const Scan = ({ onDeviceSelected }: { onDeviceSelected: (device: Device) => void
 						options={options}
 						label="Available devices"
 						defaultValue={options[0]?.label}
-						onValueChange={onChange}
+						onValueChange={(ledger: Device) => onDeviceSelected(ledger)}
 					/>
 				</View>
 			)}
 			{devices.length === 0 && (
 				<View style={styles.activityIndicatorContainer}>
 					<ActivityIndicator />
-				</View>
-			)}
-
-			{devices.length > 0 && (
-				<View style={styles.buttonContainer}>
-					<StyledButton type="confirm" onPress={onSelect} testID={'add-network-button'}>
-						Continue
-					</StyledButton>
 				</View>
 			)}
 		</View>
